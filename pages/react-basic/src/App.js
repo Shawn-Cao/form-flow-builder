247269304c3e5a297aab76f00d77fd7de84e0f32
Shawn-Cao <xiangke.cao@gmail.com>
2025-08-08 11:57:35 -0700

    try live form object

--- pages/react-basic/src/App.js
+++ pages/react-basic/src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 
 // create-react-app doesn't compile jsx in libraryes, importing partially compiled instead.
 import { FormReact as Form } from 'form-flow-builder/main.js';
-import { demoFormSpec } from 'form-flow-builder/lib/form-spec-demos.js';
-// import { multiPageForm as demoFormSpec } from 'form-flow-builder/lib/form-spec-demos.js';
+import { demoFormSpec, multiPageForm, dynamicFormSpec } from 'form-flow-builder/lib/form-spec-demos.js';
 
+let lastWorkingSpec = demoFormSpec;
 
 function App() {
-  // force form recreation to support live editing
+  // default data, also force form recreation to support live editing
   const [formSpec, setFormSpec] = useState(JSON.stringify(demoFormSpec, null, 2));
   let error, workingSpec;
   try {
     workingSpec = JSON.parse(formSpec);
+    lastWorkingSpec = workingSpec;
   } catch(err) {
     error = err;
   }
@@ -30,15 +31,22 @@ function App() {
         </a>
       </header>
 
-      <section className="App-section">
-        <p>Form specification in JSON {error ? `has error: ${error}` : 'is valid.'}</p>
-        <textarea value={formSpec} onChange={e => setFormSpec(e.target.value)} style={{height: '20em', width: '90%'}} />
+      <section className="App-section form-spec">
+        <p><label>pre-made demo specs: 
+          <select name="demos" onChange={e => setFormSpec(e.target.value)}>
+            <option value={JSON.stringify(demoFormSpec, null, 2)}>Basic</option>
+            <option value={JSON.stringify(multiPageForm, null, 2)}>Multi Page</option>
+            <option value={JSON.stringify(dynamicFormSpec, null, 2)}>Dynamic Specs</option>
+          </select>
+        </label></p>
+        <textarea name="custom" value={formSpec} onChange={e => setFormSpec(e.target.value)} style={{height: '20em', width: '90%'}} />
+        <p style={error && {color: 'red'}}>Form specification in JSON {error ? `Not updated! Error: ${error}` : 'is valid.'}</p>
       </section>
 
-      <section className="App-section">
+      <section className="App-section form-live">
         {/* TODO support templating within the form (as child?) */}
         <Form
-          formSpec={workingSpec ?? demoFormSpec}
+          formSpec={workingSpec ?? lastWorkingSpec}
           onSubmit={(formData) => window?.alert(JSON.stringify(formData, null, 2))} />
       </section>
     </div>
